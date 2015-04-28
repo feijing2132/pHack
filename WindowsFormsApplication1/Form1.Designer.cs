e91bdeffdec8f50df2c7d49c07196fd455f58155
unknown <liupo@SHACNG029WD87.ads.autodesk.com>
2015-04-28 11:31:40 +0800

    1

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonNoPay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTapcount = new System.Windows.Forms.TextBox();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonYesPay = new System.Windows.Forms.Button();
+            this.buttonDirPay = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonNoPay
+            // 
+            this.buttonNoPay.Location = new System.Drawing.Point(197, 110);
+            this.buttonNoPay.Name = "buttonNoPay";
+            this.buttonNoPay.Size = new System.Drawing.Size(75, 25);
+            this.buttonNoPay.TabIndex = 0;
+            this.buttonNoPay.Text = "Nopay";
+            this.buttonNoPay.UseVisualStyleBackColor = true;
+            this.buttonNoPay.Click += new System.EventHandler(this.buttonNoPay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "tap count";
+            // 
+            // textBoxTapcount
+            // 
+            this.textBoxTapcount.Location = new System.Drawing.Point(69, 9);
+            this.textBoxTapcount.Name = "textBoxTapcount";
+            this.textBoxTapcount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTapcount.TabIndex = 2;
+            this.textBoxTapcount.Text = "18";
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(69, 47);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumber.TabIndex = 4;
+            this.textBoxNumber.Text = "1000";
+            this.textBoxNumber.TextChanged += new System.EventHandler(this.textBoxNumber_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "number";
+            // 
+            // buttonYesPay
+            // 
+            this.buttonYesPay.Location = new System.Drawing.Point(197, 219);
+            this.buttonYesPay.Name = "buttonYesPay";
+            this.buttonYesPay.Size = new System.Drawing.Size(75, 25);
+            this.buttonYesPay.TabIndex = 5;
+            this.buttonYesPay.Text = "Yespay";
+            this.buttonYesPay.UseVisualStyleBackColor = true;
+            this.buttonYesPay.Click += new System.EventHandler(this.buttonYesPay_Click);
+            // 
+            // buttonDirPay
+            // 
+            this.buttonDirPay.Location = new System.Drawing.Point(197, 180);
+            this.buttonDirPay.Name = "buttonDirPay";
+            this.buttonDirPay.Size = new System.Drawing.Size(75, 25);
+            this.buttonDirPay.TabIndex = 6;
+            this.buttonDirPay.Text = "Dirpay";
+            this.buttonDirPay.UseVisualStyleBackColor = true;
+            this.buttonDirPay.Click += new System.EventHandler(this.buttonDirPay_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "20000";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(11, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(112, 158);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(16, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(55, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "40000";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(16, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(55, 17);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "60000";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(16, 88);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(55, 17);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "80000";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(16, 112);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(61, 17);
+            this.radioButton5.TabIndex = 11;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "100000";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(17, 132);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(61, 17);
+            this.radioButton6.TabIndex = 12;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "120000";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 251);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonDirPay);
+            this.Controls.Add(this.buttonYesPay);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxTapcount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonNoPay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonNoPay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTapcount;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonYesPay;
+        private System.Windows.Forms.Button buttonDirPay;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private int sleepTime = 20;
+        private int switchTime = 50;
+        private string totalNumber = "100";
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxNumber.Text = totalNumber;
+        }
+
+        private void buttonNoPay_Click(object sender, EventArgs e)
+        {
+            setTotalNumber();
+            preHandle();
+        }
+
+        private void buttonYesPay_Click(object sender, EventArgs e)
+        {
+            setTotalNumber();
+            preHandle();
+            downAndPay();
+        }
+
+        private void preHandle()
+        {
+            SendKeys.Send("%{TAB}");
+            Thread.Sleep(switchTime);
+            int tabCount = int.Parse(textBoxTapcount.Text);
+            for (int i = 0; i < tabCount; i++ )
+            {
+                SendKeys.Send("{TAB}");
+                Thread.Sleep(sleepTime);
+            }
+            SendKeys.Send(totalNumber);
+            Thread.Sleep(sleepTime);
+        }
+
+        private void downAndPay()
+        {
+            SendKeys.Send("{TAB}");
+            Thread.Sleep(sleepTime);
+            SendKeys.Send("{ENTER}");
+            Thread.Sleep(sleepTime);
+            //SendKeys.Send("{ENTER}");
+            //Thread.Sleep(sleepTime);
+        }
+
+        private void buttonDirPay_Click(object sender, EventArgs e)
+        {
+            setTotalNumber();
+            SendKeys.Send("%{TAB}");
+            Thread.Sleep(switchTime);
+            SendKeys.Send(totalNumber);
+            Thread.Sleep(sleepTime);
+            downAndPay();
+        }
+
+        private void setTotalNumber()
+        {
+            if (radioButton1.Checked)
+            {
+                totalNumber = radioButton1.Text;
+                return;
+            }
+
+            if (radioButton2.Checked)
+            {
+                totalNumber = radioButton2.Text;
+                return;
+            }
+
+            if (radioButton3.Checked)
+            {
+                totalNumber = radioButton3.Text;
+                return;
+            }
+
+            if (radioButton4.Checked)
+            {
+                totalNumber = radioButton4.Text;
+                return;
+            }
+
+            if (radioButton5.Checked)
+            {
+                totalNumber = radioButton5.Text;
+                return;
+            }
+
+            if (radioButton6.Checked)
+            {
+                totalNumber = radioButton6.Text;
+                return;
+            }
+        }
+
+        private void textBoxNumber_TextChanged(object sender, EventArgs e)
+        {
+            radioButton6.Text = textBoxNumber.Text;
+            radioButton6.Checked = true;
+        }
+    }
+}
